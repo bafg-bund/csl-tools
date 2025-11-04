27641cfa46c2c431e1f1c3bbca1ec5f0fc971593
lessmann <lessmann@bafg.de>
2025-11-04 14:23:06 +0100

    merge with dev

--- csl/config.py
+++ csl/config.py
@@ -9,7 +9,7 @@ ROOT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Dictionary with pairs of data source and chromatographic method
 # If data sources are added or modified, the respective mappings need to be changed here and in rtscan_functions.
-DEFAULT_PAIRS_INST_CHROM = {
+DEFAULT_PAIRS_DSOURCE_CHROM = {
     'bfg': 'bfg_nts_rp1',
     'uba': 'uba_nts_rp1',
     'lfuby': 'lfuby_nts_rp1',
